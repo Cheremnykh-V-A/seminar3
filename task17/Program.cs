0b96144ab8e3656a74a04af752e642d54b9a3cfe
Vadim Cheremnykh <chere96@yandex.ru>
2023-02-17 21:58:09 +0400

    add task17

---
+++ task17/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+Console.WriteLine("Введите координаты Х");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находится в первой координатной четверти");
+}
+
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("Точка находится во второй координатной четверти");
+}
+
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в третьей координатной четверти");
+}
+
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в четвёртой координатной четверти");
+}
+
+else
+{
+    Console.WriteLine("Невозможно определить координатную четверть");
+}
